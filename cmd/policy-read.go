cfcb6ea07d07c00312e1a6ef730e51953687ee38
aloksharma20 <alokkumar.sharma@accuknox.com>
2022-09-29 00:39:41 +0530

    Apply policy from .yaml file

--- cmd/policy-read.go
+++ cmd/policy-read.go
@@ -3,41 +3,50 @@ package cmd
 import (
 	"fmt"
 	"io/ioutil"
+	"path/filepath"
 
 	p "github.com/datasage-io/datasage-cli/policy-ops"
 	pb "github.com/datasage-io/datasage/src/proto/policy"
 	"github.com/spf13/cobra"
 )
 
+// To Decalre Policy
+var fileURL string
+
+// To Store Policy Content
 var policy pb.ReadPolicyYAMLFile
 
-//class represents
+// class represents
 var readPolicycmd = &cobra.Command{
 	Use:   "policy",
 	Short: "Policy Commands For Manipulating Policy in Datasage",
 	Long:  ` Policy Commands to do Manipulation on Policy in Datasage`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		//To Store File
-		var file string
-		if len(args) != 0 {
-			file = args[0]
-		}
-		readFile, err := ioutil.ReadFile(file)
-		if err != nil {
-			return err
+		//To Check file extension
+		ext := filepath.Ext(fileURL)
+		if ext == ".yaml" {
+			//Read File Path
+			data, err := ioutil.ReadFile(fileURL)
+			if err != nil {
+				fmt.Println("File reading error", err)
+				return nil
+			}
+
+			//To Store in Request
+			policy.Policy = string(data)
+
+			//Send to Server
+			response, err := p.ReadPolicy(policy)
+			fmt.Println(response.GetMessage())
+		} else {
+			fmt.Println("Policy file should be in .yaml extension")
 		}
 
-		//To Store in Request
-		policy.Policy = string(readFile)
-
-		//Send to Server
-		response, err := p.ReadPolicy(policy)
-		fmt.Println(response.GetMessage())
 		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(readPolicycmd)
-	readPolicycmd.Flags().StringVarP(&policy.Policy, "f", "f", "", "Read Policy YAML File")
+	readPolicycmd.Flags().StringVarP(&fileURL, "f", "f", "", "Read Policy YAML File")
 }
